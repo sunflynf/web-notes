7c58434f0a94414f8e8df56ed156a1e3b86a08e6
Phi Võ <75079929+sunflynf@users.noreply.github.com>
2024-06-12 07:15:39 +0000

    .

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/workspaces/web-notes/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/web-notes/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/web-notes/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/web-notes/src/css/custom.css"),
-];
